cb7986c5d325d3a16c71b16acb584c441dc51f82
anderdavid <anderdavid86@gmail.com>
2021-08-17 14:44:48 -0500

    overview bulma

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
+import Login from './components/login';
 
 function App() {
   return (
-    <div className="container">
-      <h1 className="title">
-        Hello World Reto NativApps
-      </h1>
-      <p className="subtitle">
-        Hello World<strong>Bulma</strong>!
-      </p>
-      <div class="buttons">
-      <button class="button is-primary">Primary</button>
-      <button class="button is-link">Link</button>
-    </div>
-
-      <div class="buttons">
-        <button class="button is-info">Info</button>
-        <button class="button is-success">Success</button>
-        <button class="button is-warning">Warning</button>
-        <button class="button is-danger">Danger</button>
-      </div>
+    <div>
+      <Login></Login>
     </div>
   );
 }
